ff54ca91d9246444dd6d6f4c26bd8534245446cb
yugal1107 <yugal1107@gmail.com>
2024-08-03 22:42:49 +0530

    Initialised project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,32 @@
+import { Input } from "@nextui-org/react";
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="flex h-dvh justify-center items-center">
+      <div className="rounded-2xl flex flex-col p-10 gap-10 bg-yellow-100">
+        <form action="submit">
+          <Input
+            type="email"
+            label="Email"
+            placeholder="you@example.com"
+            labelPlacement="outside"
+            //   startContent={
+            //     <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+            //   }
+          />
+          <Input
+            labelPlacement="outside"
+            label="Password"
+            placeholder="Enter your password"
+            type={"password"}
+            className="max-w-xs"
+          />
+        <button type="submit" className="btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
