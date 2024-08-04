f9a19c52107a89bb775e2e990a21807873ff5e73
yugal1107 <yugal1107@gmail.com>
2024-08-04 15:30:18 +0530

    Authentication Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import NavbarUI from "../components/Navbar/NavbarUI";
+import { useAuth } from "../context/authContext";
 
 const Home = () => {
-  return <div>
-    <NavbarUI />
-  </div>;
+  const { currentUser } = useAuth();
+  console.log("Home.jsx: user: ", currentUser);
+  // console.log(user);
+
+  return (
+    <div>
+      <NavbarUI />
+    </div>
+  );
 };
 
 export default Home;
