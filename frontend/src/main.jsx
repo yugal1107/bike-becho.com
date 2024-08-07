0a0a6959396f0b7446c30f57fbdac14c74006538
yugal1107 <yugal1107@gmail.com>
2024-08-07 14:59:23 +0530

    Started authentication again

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
-// import { UserProvider } from "./context/authContext";
+import { UserProvider } from "./context/authContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NextUIProvider>
-      {/* <UserProvider> */}
+      <UserProvider>
         <App />
-      {/* </UserProvider> */}
+      </UserProvider>
     </NextUIProvider>
   </React.StrictMode>
 );
