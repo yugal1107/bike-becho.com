cd612bad740bb6e10a2c59d593bc5d2b7da9e50d
YogeshJat8965 <yogeshjat8965@gmail.com>
2024-11-10 17:58:38 +0530

    Submit deleted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,14 +26,14 @@ function App() {
   } = useForm();
   return (
     <>
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
+    {/* <form onSubmit={handleSubmit((data) => console.log(data))}>
       <input {...register('firstName')} />
       <input {...register('lastName', { required: true })} />
       {errors.lastName && <p>Last name is required.</p>}
       <input {...register('age', { pattern: /\d+/ })} />
       {errors.age && <p>Please enter number for age.</p>}
       <input type="submit" />
-    </form>
+    </form> */}
       <Router>
         <NavbarUI />
         <Routes>
